705b6dbb8bbb12f3da22a8dbf5927276970e375d
warmastr <jargyle80@yahoo.com>
2022-04-19 18:05:10 -0500

    created initial flexbox structure

---
+++ app.js
@@ -0,0 +1,17 @@
+// set up the html structure for the design
+const outerContainer = document.querySelector(.containerOuter);
+const innerContainer = document.querySelector('.containerInner');
+const controls = document.querySelector('.controls');
+const heading = document.querySelector('h1');
+const brushColor = document.querySelector('.colorBrush');
+const backgroundColor = document.querySelector('.colorBg');
+const shadeBtnContainer = document.querySelector('.btnContainer');
+const darkBtn = document.querySelector('.darken');
+const lightBtn = document.querySelector('.lighten');
+const gridSize = document.querySelector('.gridSize');
+const gridToggle = document.querySelector('.gridToggle');
+const gridContainer = document.querySelector('.gridContainer');
+const grid = document.createElement('div');
+
+grid = document.setAttribute('class', 'grid');
+
