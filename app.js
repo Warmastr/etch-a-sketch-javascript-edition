793c483ee66f908fb3dc1b3a65f353666e539b42
warmastr <jargyle80@yahoo.com>
2022-04-20 09:38:53 -0500

    initial styling for outer container complete

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // set up the html structure for the design
-const outerContainer = document.querySelector(.containerOuter);
+/* const outerContainer = document.querySelector('.containerOuter');
 const innerContainer = document.querySelector('.containerInner');
 const controls = document.querySelector('.controls');
 const heading = document.querySelector('h1');
@@ -8,10 +8,10 @@ const backgroundColor = document.querySelector('.colorBg');
 const shadeBtnContainer = document.querySelector('.btnContainer');
 const darkBtn = document.querySelector('.darken');
 const lightBtn = document.querySelector('.lighten');
-const gridSize = document.querySelector('.gridSize');
-const gridToggle = document.querySelector('.gridToggle');
+const gridSize = document.querySelector('#gridSize');
+const gridToggle = document.querySelector('#gridToggle');
 const gridContainer = document.querySelector('.gridContainer');
 const grid = document.createElement('div');
+const clear = document.querySelector('.clear');
 
-grid = document.setAttribute('class', 'grid');
-
+grid = document.setAttribute('class', 'grid'); */
